0e4719ba81df40336a4c135c82cf0d6a58c1ea49
Paulo-Viana <paulodev1612@gmail.com>
2021-11-03 09:46:28 -0300

    configuração inicial do projeto e primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div
+      className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-red-500 to-blue-500`}
+    >
+      <h1>Olá Mundo!</h1>
+    </div>
+  )
+}
+
+export default Home
